b6ae452b5358fb78c58a50813467fe516f79cd5d
shuaitro <shuaitro@retailsbs.com>
2020-12-24 02:31:20 -0300

    digimon module is added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { HttpModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WeatherModule } from './weather/weather.module';
+import { DigimonModule } from './digimon/digimon.module';
 
 @Module({
-  imports: [WeatherModule,HttpModule],
+  imports: [WeatherModule,HttpModule, DigimonModule],
   controllers: [AppController],
   providers: [AppService],
 })
